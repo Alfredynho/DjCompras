d7bb0d908d0522cdd79e7d6d83acbc3792996c89
alfredo <callizayagutierrezalfredo@gmail.com>
2016-04-05 22:23:24 -0400

    template productos on base de datos

--- ccompras/apps/ventas/views.py
+++ ccompras/apps/ventas/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from ccompras.apps.ventas.forms import addProductFrom
+from ccompras.apps.ventas.models import producto
+def add_product_view(request):
+	if request.method == "POST":
+			form = addProductFrom(request.POST)
+			info = "inicializando"
+			if form.is_valid():
+				nombre = form.cleaned_data['nombre']
+				descripcion = form.cleaned_data['descripcion']
+				p = producto()
+				p.nombre = nombre
+				p.descripcion = descripcion
+				p.status = True
+				p.save()
+				info = "Se guardo satisfactoriamente"
+			else:
+				info = "Informacion con datos incorrectos"
+			form = addProductFrom()
+			ctx = {'form':form, 'informacion':info}
+			return render_to_response('ventas/addProducto.html',ctx,context_instance=RequestContext(request))
+	else:
+			form = addProductFrom()
+			ctx = {'form':form}
+	return render_to_response('ventas/addProducto.html',ctx,context_instance=RequestContext(request))
 
-# Create your views here.
--- ccompras/urls.py
+++ ccompras/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
 	url(r'^',include('ccompras.apps.home.urls')),
-    url(r'^admin/', admin.site.urls),
+	url(r'^',include('ccompras.apps.ventas.urls')),
+	url(r'^admin/', admin.site.urls),
 ]
